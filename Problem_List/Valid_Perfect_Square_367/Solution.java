a316cba3395a12994b4e7626110edc1b37df052f
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-07-17 07:42:28 +0300

    Added Valid_Perfect_Square_367

---
+++ Problem_List/Valid_Perfect_Square_367/Solution.java
@@ -0,0 +1,29 @@
+package Problem_List.Valid_Perfect_Square_367;
+
+class Solution {
+
+
+    public boolean isPerfectSquare ( int num ) {
+        if ( num < 1 ) {
+            return false; // Відразу повертаємо false, якщо num менше 1
+        }
+
+        long left = 1, right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if ( square == num ) {
+                return true; // Якщо знайдено perfect square, повертаємо true
+            } else if ( square < num ) {
+                left = mid + 1; // Переміщуємо ліву межу вправо
+            } else {
+                right = mid - 1; // Переміщуємо праву межу вліво
+            }
+        }
+
+        return false; // Якщо не знайдено perfect square, повертаємо false
+    }
+
+}
