151e809f5eafa6fce7669da00da1df952f491690
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-08-13 08:23:43 +0300

    Added Q37_Sudoku_Solver

---
+++ Problem_List/Q37_Sudoku_Solver/Solution.java
@@ -0,0 +1,69 @@
+package Problem_List.Q37_Sudoku_Solver;
+
+public class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    // Пробуємо кожне число від 1 до 9
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, row, col, c)) {
+                            board[row][col] = c;
+
+                            // Якщо дошка вирішена, повертаємо true
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Інакше повертаємося назад
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Перевірка рядка
+            if (board[row][i] == c) return false;
+            // Перевірка стовпця
+            if (board[i][col] == c) return false;
+            // Перевірка 3x3 підсітки
+            if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == c) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        sol.solveSudoku(board);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
