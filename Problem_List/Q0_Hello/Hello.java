5f33d9f6a0894df277633c8983c903fe01cbcbd5
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-08-28 08:10:07 +0300

    Added Q52_N_Queens_II

--- Problem_List/Q0_Hello/Hello.java
+++ Problem_List/Q0_Hello/Hello.java
@@ -1,4 +1,4 @@
-package Problem_List.Hello;
+package Problem_List.Q0_Hello;
 
 public class Hello {
     public static void main ( String[] args ) {
---
+++ Problem_List/Q52_N_Queens_II/Solution.java
@@ -0,0 +1,56 @@
+package Problem_List.Q52_N_Queens_II;
+
+import java.util.*;
+
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+        backtrack(board, 0);
+        return count;
+    }
+
+    private void backtrack(char[][] board, int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
