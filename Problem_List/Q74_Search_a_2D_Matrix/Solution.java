894c3564fc56ba6475032c462f44eb798af0cf05
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-09-15 11:54:31 +0300

    Added Q74_Search_a_2D_Matrix

---
+++ Problem_List/Q74_Search_a_2D_Matrix/Solution.java
@@ -0,0 +1,29 @@
+package Problem_List.Q74_Search_a_2D_Matrix;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n];
+
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
