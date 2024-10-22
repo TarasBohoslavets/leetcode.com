97773bc371289145e8475b5f56222e48cbc2ca62
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-10-22 23:36:35 +0300

    Q122_Best_Time_to_Buy_and_Sell_Stock_II

---
+++ Problem_List/Q122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,13 @@
+package Problem_List.Q122_Best_Time_to_Buy_and_Sell_Stock_II;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
