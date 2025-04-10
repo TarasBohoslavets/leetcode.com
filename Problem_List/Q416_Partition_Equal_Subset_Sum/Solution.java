a53b15b78e8ddebb3a0c6af34bb2a7e70b0cfa00
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2025-04-10 07:51:47 +0300

    Q416_Partition_Equal_Subset_Sum

---
+++ Problem_List/Q416_Partition_Equal_Subset_Sum/Solution.java
@@ -0,0 +1,25 @@
+package Problem_List.Q416_Partition_Equal_Subset_Sum;
+
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (totalSum % 2 != 0) return false;
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+
+}
