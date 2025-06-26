45346e503d65a8b4603a3cbb74794a77510d32d4
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2025-06-26 16:33:38 +0300

    Q511_Game_Play_Analysis_I

---
+++ Problem_List/Q511_Game_Play_Analysis_I/Solution.java
@@ -0,0 +1,11 @@
+package Problem_List.Q511_Game_Play_Analysis_I;
+
+public class Solution {
+    /*
+-- Write your PostgreSQL query statement below
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id
+ORDER BY (player_id) ASC
+    */
+}
