9a75709d79a3124050936aca3b1d51b2040a35f2
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2025-07-15 22:49:00 +0300

    Q521_Longest_Uncommon_Subsequence_I

---
+++ Q521_Longest_Uncommon_Subsequence_I/Solution.java
@@ -0,0 +1,8 @@
+package Q521_Longest_Uncommon_Subsequence_I;
+
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
