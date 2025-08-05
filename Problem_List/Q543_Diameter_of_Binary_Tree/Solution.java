bc6729f0b3972f25a11cc16cc574e0430994abe7
BTB <bogoslavetstaras@gmail.com>
2025-08-05 07:56:08 +0300

    Q543_Diameter_of_Binary_Tree

---
+++ Problem_List/Q543_Diameter_of_Binary_Tree/Solution.java
@@ -0,0 +1,44 @@
+package Problem_List.Q543_Diameter_of_Binary_Tree;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return maxDiameter;
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftDepth = depth(node.left);
+        int rightDepth = depth(node.right);
+
+        // Оновлюємо діаметр: кількість ребер між двома найглибшими вузлами
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+
+        // Повертаємо глибину поточного вузла
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
