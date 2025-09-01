ad157723644f097b342ea1b3d91003cc3412fcf6
BTB <bogoslavetstaras@gmail.com>
2025-09-01 10:00:13 +0300

    Q572_Subtree_of_Another_Tree

---
+++ Problem_List/Q572_Subtree_of_Another_Tree/Solution.java
@@ -0,0 +1,40 @@
+package Problem_List.Q572_Subtree_of_Another_Tree;
+
+import java.util.Arrays;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode() {
+	}
+	
+	TreeNode(int val) {
+		this.val = val;
+	}
+	
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class Solution {
+	public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+		if (root == null) return false;
+		
+		if (isSameTree(root, subRoot)) return true;
+		
+		return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+	}
+	
+	private boolean isSameTree(TreeNode a, TreeNode b) {
+		if (a == null && b == null) return true;
+		if (a == null || b == null) return false;
+		if (a.val != b.val) return false;
+		
+		return isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
+	}
+}
