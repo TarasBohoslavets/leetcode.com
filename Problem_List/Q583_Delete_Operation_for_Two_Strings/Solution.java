e3c623b3104d62eb0e8fe90b68e478ec196c364c
BTB <bogoslavetstaras@gmail.com>
2025-09-05 10:20:29 +0300

    Q583_Delete_Operation_for_Two_Strings

---
+++ Problem_List/Q583_Delete_Operation_for_Two_Strings/Solution.java
@@ -0,0 +1,22 @@
+package Problem_List.Q583_Delete_Operation_for_Two_Strings;
+
+class Solution {
+	public int minDistance(String word1, String word2) {
+		int m = word1.length(), n = word2.length();
+		int[][] dp = new int[m + 1][n + 1];
+		
+		// Побудова таблиці LCS
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		
+		int lcs = dp[m][n];
+		return (m - lcs) + (n - lcs);
+	}
+}
