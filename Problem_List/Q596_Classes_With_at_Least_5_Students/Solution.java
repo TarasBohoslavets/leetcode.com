a00655ecf431abb07ed33f651f03f67af29cc0de
BTB <bogoslavetstaras@gmail.com>
2025-09-26 13:37:21 +0300

    Q596_Classes_With_at_Least_5_Students

---
+++ Problem_List/Q596_Classes_With_at_Least_5_Students/Solution.java
@@ -0,0 +1,11 @@
+package Problem_List.Q596_Classes_With_at_Least_5_Students;
+
+public class Solution {
+	/*
+	-- Write your PostgreSQL query statement below
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) > 4;
+	 */
+}
