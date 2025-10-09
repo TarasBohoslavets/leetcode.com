577b7bd79a4e2a764242b91f7652a7cfb799ff3f
BTB <bogoslavetstaras@gmail.com>
2025-10-09 09:28:38 +0300

    Q610_Triangle_Judgement

---
+++ Problem_List/Q610_Triangle_Judgement/Solution.java
@@ -0,0 +1,14 @@
+package Problem_List.Q610_Triangle_Judgement;
+
+public class Solution {
+	/*
+	-- Write your PostgreSQL query statement below
+SELECT
+  x, y, z,
+  CASE
+    WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+    ELSE 'No'
+  END AS triangle
+FROM Triangle;
+	 */
+}
