f9f6e05df8a59aa2f247bccf4123cd5313e187b1
BTB <bogoslavetstaras@gmail.com>
2025-10-23 10:38:15 +0300

    Q627_Swap_Salary

---
+++ Problem_List/Q627_Swap_Salary/Solution.java
@@ -0,0 +1,13 @@
+package Problem_List.Q627_Swap_Salary;
+
+public class Solution {
+	/*
+	-- Write your PostgreSQL query statement below
+UPDATE Salary
+SET sex =
+    CASE
+        WHEN sex = 'f' THEN 'm'
+        ELSE 'f'
+    END;
+	 */
+}
