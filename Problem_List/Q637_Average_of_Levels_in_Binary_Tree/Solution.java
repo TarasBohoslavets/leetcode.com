2bdf357ebb54e810094307f8cc5e9329c339793d
BTB <bogoslavetstaras@gmail.com>
2025-10-31 11:23:21 +0200

    Q637_Average_of_Levels_in_Binary_Tree

---
+++ Problem_List/Q637_Average_of_Levels_in_Binary_Tree/Solution.java
@@ -0,0 +1,42 @@
+package Problem_List.Q637_Average_of_Levels_in_Binary_Tree;
+
+import java.util.*;
+
+class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+	public List<Double> averageOfLevels(TreeNode root) {
+		List<Double> result = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedList<>();
+		
+		queue.offer(root);
+		
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			double sum = 0;
+			
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				sum += node.val;
+				
+				if (node.left != null) queue.offer(node.left);
+				if (node.right != null) queue.offer(node.right);
+			}
+			
+			result.add(sum / size);
+		}
+		
+		return result;
+	}
+}
