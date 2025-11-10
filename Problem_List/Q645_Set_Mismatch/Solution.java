3a0422fc74f8e514550b25ee1b51997c3b46713e
BTB <bogoslavetstaras@gmail.com>
2025-11-10 10:36:14 +0200

    Q645_Set_Mismatch

---
+++ Problem_List/Q645_Set_Mismatch/Solution.java
@@ -0,0 +1,20 @@
+package Problem_List.Q645_Set_Mismatch;
+
+class Solution {
+	public int[] findErrorNums(int[] nums) {
+		int[] count = new int[nums.length + 1];
+		int duplicate = -1, missing = -1;
+		
+		for (int num : nums) {
+			count[num]++;
+		}
+		
+		for (int i = 1; i < count.length; i++) {
+			if (count[i] == 2) duplicate = i;
+			else if (count[i] == 0) missing = i;
+		}
+		
+		return new int[]{duplicate, missing};
+	}
+}
+
