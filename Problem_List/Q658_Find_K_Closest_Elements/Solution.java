fcbf1ce8e8f04837b6a0063f084dfa3029775f83
BTB <bogoslavetstaras@gmail.com>
2025-11-25 09:04:19 +0200

    Q658_Find_K_Closest_Elements

---
+++ Problem_List/Q658_Find_K_Closest_Elements/Solution.java
@@ -0,0 +1,24 @@
+package Problem_List.Q658_Find_K_Closest_Elements;
+
+import java.util.*;
+
+class Solution {
+	public List<Integer> findClosestElements(int[] arr, int k, int x) {
+		int left = 0, right = arr.length - 1;
+		
+		// Звужуємо вікно до розміру k
+		while (right - left + 1 > k) {
+			if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		
+		List<Integer> result = new ArrayList<>();
+		for (int i = left; i <= right; i++) {
+			result.add(arr[i]);
+		}
+		return result;
+	}
+}
